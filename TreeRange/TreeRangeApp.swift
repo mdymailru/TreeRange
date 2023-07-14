d64e80f3fddbc566d92a09d8f2036074ba8641ff
Dmitry Martynov <mdymailru@gmail.com>
2023-07-14 22:46:17 +0300

    Initial Commit

---
+++ TreeRange/TreeRangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreeRangeApp.swift
+//  TreeRange
+//
+//  Created by Dmitry Martynov on 14.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct TreeRangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
